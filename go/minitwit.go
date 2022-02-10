52a1da87fe5349ee287274561199645a900d1ac2
Christoffer Gram <chkg@itu.dk>
2022-02-10 11:58:23 +0100

    Open Sqlite3 Connection in Go (#36)
    * First attempt at connecting to db with go. Currently explodes
    
    * Commit for Jakob
    
    * Start Webserver With Trying to Open Sqlite3
    
    Co-authored-by: Oskar <oskar.loenne@gmail.com>
    Co-authored-by: Oskar Breindahl <osbr@itu.dk>

--- go/minitwit.go
+++ go/minitwit.go
@@ -1,19 +1,27 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func YourHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Gorilla!\n"))
 }
 
+const dbFile = "../minitwit.db"
+
 func main() {
+	_, err := sql.Open("sqlite3", dbFile)
+	if err != nil {
+		log.Fatalf("Failed to connect to the database with error: %v", err)
+	}
+
 	r := mux.NewRouter()
-	// Routes consist of a path and a handler function.
 	r.HandleFunc("/", YourHandler)
 
 	// Bind to a port and pass our router in
