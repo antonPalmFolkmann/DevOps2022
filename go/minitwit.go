b19f36333cce8a1112d8064ad1163734e7aeca71
Christoffer Gram <chkg@itu.dk>
2022-02-11 21:44:35 +0100

    Refactor/db 5 6 7 (#37)
    * Add ConnectDb and InitDb
    
    The sql.Tx type is the closest I could find with regards to python's
    cursor type.
    
    Entirely untested
    
    * Add QueryDb
    
    Not tested at all
    
    * Make QueryDb Work

--- go/minitwit.go
+++ go/minitwit.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -9,19 +10,79 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+// Configuration
+const (
+	DATABASE = "../minitwit.db"
+)
+
+var (
+	// Create our little application :)
+	r  *mux.Router = mux.NewRouter()
+	db *sql.DB     = ConnectDb()
+)
+
+// ConnectDb returns a new connection to the database
+func ConnectDb() *sql.DB {
+	db, _ := sql.Open("sqlite3", DATABASE)
+	return db
+}
+
+// InitDb creates the database tables
+func InitDb() {
+	db := ConnectDb()
+	defer db.Close()
+
+	query, _ := ioutil.ReadFile("../schema.sql")
+
+	tx, _ := db.Begin()
+	stmt, _ := tx.Prepare(string(query))
+	stmt.Exec()
+	tx.Commit()
+}
+
+// Hack for an array of maps in golang:
+// https://stackoverflow.com/questions/47130003/how-can-i-declare-list-of-maps-in-golang
+type M map[string]interface{}
+
+// Queries the database and returns a list of maps
+func QueryDb(query string, one bool, args ...interface{}) []M {
+	rv := make([]M, 0)
+	rows, _ := db.Query(query, args...)
+	cols, _ := rows.Columns()
+	for rows.Next() {
+		// Solution for storing results in map adapted from: https://kylewbanks.com/blog/query-result-to-map-in-golang
+		columns := make([]interface{}, len(cols))
+		columnPointers := make([]interface{}, len(cols))
+
+		for i := range columns {
+			columnPointers[i] = &columns[i]
+		}
+
+		_ = rows.Scan(columnPointers...)
+
+		row := make(M)
+		for i, colName := range cols {
+			val := columnPointers[i].(*interface{})
+			row[colName] = *val
+		}
+
+		rv = append(rv, row)
+	}
+
+	if len(rv) == 0 {
+		return nil
+	} else if one {
+		return rv[:1]
+	} else {
+		return rv
+	}
+}
+
 func YourHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Gorilla!\n"))
 }
 
-const dbFile = "../minitwit.db"
-
 func main() {
-	_, err := sql.Open("sqlite3", dbFile)
-	if err != nil {
-		log.Fatalf("Failed to connect to the database with error: %v", err)
-	}
-
-	r := mux.NewRouter()
 	r.HandleFunc("/", YourHandler)
 
 	// Bind to a port and pass our router in
