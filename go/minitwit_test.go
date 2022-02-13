3e53d577ca7e5eaec91116062ba165c646b4a02a
Christoffer Gram <chkg@itu.dk>
2022-02-13 15:52:31 +0100

    Add SetUp (#42)

--- go/minitwit_test.go
+++ go/minitwit_test.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"testing"
 
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/stretchr/testify/assert"
 )
 
+func SetUp() {
+	db, _ = sql.Open("sqlite3", ":memory:")
+	// Use httptest package instead of minitwit.app.test_client()
+	DATABASE = ":memory:"
+	InitDb()
+}
+
 func TestSomething(t *testing.T) {
+	SetUp()
+
 	assert.True(t, true, "True is true!")
 }
