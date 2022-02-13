a8120c480d883b0058f29cced1b2e1322ee6c944
Anton Palm Folkmann <69316986+antonPalmFolkmann@users.noreply.github.com>
2022-02-13 15:04:24 +0100

    Refactor/dev 8 20 (#39)
    * Added function signatures. Code body is TBD.
    
    * Added code to AddMessage and GetUserId.
    
    * Added Redirection
    
    * Parse Form Data of HTTP Request
    
    Adapted from code found in:
    https://medium.com/@edwardpie/processing-form-request-data-in-golang-2dff4c2441be=
    
    * Remove All Warnings + Compile Errors
    
    Co-authored-by: Christoffer Gram <chkg@itu.dk>

--- go/minitwit.go
+++ go/minitwit.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 	_ "github.com/mattn/go-sqlite3"
@@ -17,8 +19,9 @@ const (
 
 var (
 	// Create our little application :)
-	r  *mux.Router = mux.NewRouter()
-	db *sql.DB     = ConnectDb()
+	r       *mux.Router = mux.NewRouter()
+	db      *sql.DB     = ConnectDb()
+	session map[string]string
 )
 
 // ConnectDb returns a new connection to the database
@@ -76,6 +79,41 @@ func QueryDb(query string, one bool, args ...interface{}) []M {
 	} else {
 		return rv
 	}
+}
+
+// Registers a new message for the user.
+func AddMessage(w http.ResponseWriter, r *http.Request) {
+	if _, found := session["user_id"]; !found {
+		log.Fatalln("Abort 401")
+	}
+
+	r.ParseForm()
+	if _, found := r.Form["text"]; found {
+		insertMessageSQL := "INSERT INTO message (author_id, text, pub_date, flagged) VALUES (%s,%s,%s,0)"
+		statement, err := db.Prepare(insertMessageSQL) // Avoid SQL injections
+
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		_, err = statement.Exec(session["user_id"], r.Form["text"], time.Now)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		http.Redirect(w, r, "http:localhost:8080/timeline", http.StatusFound)
+	}
+}
+
+// Convenience method to look up the id for a username.
+func GetUserId(username string) (*int, error) {
+	var usernameResult int
+	// Query for a value based on a single row.
+	if err := db.QueryRow("SELECT user_id from user where id = ?", username).Scan(&username); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, fmt.Errorf("GetUserId %s: unknown username", username)
+		}
+		return nil, fmt.Errorf("GetUserId %s failed", username)
+	}
+	return &usernameResult, nil
 }
 
 func YourHandler(w http.ResponseWriter, r *http.Request) {
