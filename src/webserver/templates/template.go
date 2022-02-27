19c2a3830fa6975693a2f908f7a53b3bd557d769
Anton Palm Folkmann <69316986+antonPalmFolkmann@users.noreply.github.com>
2022-02-27 17:47:20 +0100

    Clean-up of template.go.

--- src/webserver/templates/template.go
+++ src/webserver/templates/template.go
@@ -10,28 +10,6 @@ import (
 	"time"
 )
 
-var user M
-
-// Hack for an array of maps in golang:
-// https://stackoverflow.com/questions/47130003/how-can-i-declare-list-of-maps-in-golang
-type M map[string]interface{}
-
-// Return the gravatar image for the given email address.
-// Converting string to bytes: https://stackoverflow.com/questions/42541297/equivalent-of-pythons-encodeutf8-in-golang
-// Converting bytes to hexadecimal s%}tring: https://pkg.go.dev/encoding/hex#EncodeToString
-func GravatarUrl(email interface{}, size int) string {
-	strEmail := email.(string)
-	return fmt.Sprintf("http://www.gravatar.com/avatar/%s?d=identicon&s=%d",
-		hex.EncodeToString([]byte(strings.ToLower(strings.TrimSpace(strEmail)))), size)
-}
-
-func initTemplate(name string) *template.Template {
-	return template.New(name).Funcs(template.FuncMap{
-		"gravatar":       func(size int, email interface{}) string { return GravatarUrl(email, size) },
-		"datetimeformat": FormatDatetime,
-	})
-}
-
 func AddMessageTemplate(w http.ResponseWriter, data interface{}) {
 	tmpl, err := initTemplate("addmessage.html").ParseFiles("templates/layout.html", "templates/addmessage.html")
 	if err != nil {
@@ -78,7 +56,23 @@ func TimelineTemplate(w http.ResponseWriter, data interface{}) {
 	}
 }
 
+// Return the gravatar image for the given email address.
+// Converting string to bytes: https://stackoverflow.com/questions/42541297/equivalent-of-pythons-encodeutf8-in-golang
+// Converting bytes to hexadecimal s%}tring: https://pkg.go.dev/encoding/hex#EncodeToString
+func initTemplate(name string) *template.Template {
+	return template.New(name).Funcs(template.FuncMap{
+		"gravatar":       func(size int, email interface{}) string { return GravatarUrl(email, size) },
+		"datetimeformat": FormatDatetime,
+	})
+}
+
 func FormatDatetime(timestamp int64) string {
 	timeUnix := time.Unix(timestamp, 0)
 	return timeUnix.Format("2006-01-02 15:04")
+}
+
+func GravatarUrl(email interface{}, size int) string {
+	strEmail := email.(string)
+	return fmt.Sprintf("http://www.gravatar.com/avatar/%s?d=identicon&s=%d",
+		hex.EncodeToString([]byte(strings.ToLower(strings.TrimSpace(strEmail)))), size)
 }
