903b68fdd9a5403bda2357f66c55705f69a957dd
Christoffer Gram <chkg@itu.dk>
2022-03-08 23:33:15 +0100

    Format server.go

--- src/webserver/server.go
+++ src/webserver/server.go
@@ -4,14 +4,13 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/antonPalmFolkmann/DevOps2022/database"
 	"github.com/antonPalmFolkmann/DevOps2022/minitwit"
 	"github.com/antonPalmFolkmann/DevOps2022/simulator"
 	"github.com/gorilla/mux"
 )
 
 func main() {
-	
+
 	go func() {
 		r := mux.NewRouter()
 		simulator.SetupRoutes(r)
@@ -24,13 +23,13 @@ func main() {
 	r := mux.NewRouter()
 	minitwit.SetupRoutes(r)
 	log.Fatalln(http.ListenAndServe(":8080", r))
-	
+
 	/*
-	var db = database.GetConnection()
-	services.SetDB(db)
-	var appRouter = routes.CreateRouter()
-	
-	log.Println("Listening on Port 8080")
-	log.Fatal(http.ListenAndServe(":8080", appRouter))
+		var db = database.GetConnection()
+		services.SetDB(db)
+		var appRouter = routes.CreateRouter()
+
+		log.Println("Listening on Port 8080")
+		log.Fatal(http.ListenAndServe(":8080", appRouter))
 	*/
 }
