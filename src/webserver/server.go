186a1115653fd1ff3e3b83fc2ea636b8aa2e9eb0
Anton Palm Folkmann <69316986+antonPalmFolkmann@users.noreply.github.com>
2022-03-08 14:06:56 +0100

    Minor edits in startup

--- src/webserver/server.go
+++ src/webserver/server.go
@@ -5,13 +5,13 @@ import (
 	"net/http"
 
 	"github.com/antonPalmFolkmann/DevOps2022/database"
-	"github.com/antonPalmFolkmann/DevOps2022/routes"
-	"github.com/antonPalmFolkmann/DevOps2022/services"
+	"github.com/antonPalmFolkmann/DevOps2022/minitwit"
+	"github.com/antonPalmFolkmann/DevOps2022/simulator"
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	
-	/*
 	go func() {
 		r := mux.NewRouter()
 		simulator.SetupRoutes(r)
@@ -24,12 +24,13 @@ func main() {
 	r := mux.NewRouter()
 	minitwit.SetupRoutes(r)
 	log.Fatalln(http.ListenAndServe(":8080", r))
-	*/
-
+	
+	/*
 	var db = database.GetConnection()
 	services.SetDB(db)
 	var appRouter = routes.CreateRouter()
 	
 	log.Println("Listening on Port 8080")
 	log.Fatal(http.ListenAndServe(":8080", appRouter))
+	*/
 }
