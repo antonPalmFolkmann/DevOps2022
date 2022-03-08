dd042e30d1cc59ecac9188adf314088574adf322
Anton Palm Folkmann <69316986+antonPalmFolkmann@users.noreply.github.com>
2022-03-08 09:25:36 +0100

    Interfaces for service layer created.

--- src/webserver/storage/ServiceInterfaces.go
+++ src/webserver/storage/ServiceInterfaces.go
@@ -2,15 +2,21 @@ package storage
 
 import "time"
 
-type Store interface {
+type IUser interface {
 	ReadUserById(id int64) *User
 	ReadUserByUsername(username string) *User
 	CreateUser(username string, email string, pw_hash string) error
 	GetUserIdByUsername(username string) int64
+	DeleteUser(id int64) error
+}
 
+type IFollows interface{
 	CreateFollower(whoId int64, whomId int64)
 	DeleteFollower(whoId int64, whomId int64)
+}
 
+
+type IMessage interface {
 	CreateMessage(authorId int64, text string, pubDate *time.Time, flagged bool) error
 	ReadMessages() []*Message
 	ReadMessagesByFlagged(isFlagged bool) []*Message
