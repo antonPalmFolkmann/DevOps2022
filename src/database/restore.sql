5920c461fea6625eb81bc5e783c22f2556a89d1a
Christoffer Gram <chkg@itu.dk>
2022-03-10 20:02:49 +0100

    Make Restore Fit New Schema

--- src/database/restore.sql
+++ src/database/restore.sql
@@ -1,37 +1,39 @@
-drop table if exists "user";
-create table "user" (
+drop table if exists users cascade;
+create table "users" (
   user_id serial primary key,
   username varchar(100) not null,
   email varchar(100) not null,
   pw_hash varchar(100) not null
 );
 
-drop table if exists follower;
+drop table if exists followers cascade;
 create table follower (
-  who_id integer,
-  whom_id integer
+  user_id integer,
+  whom_id integer,
+
+  primary key (user_id, whom_id)
 );
 
-drop table if exists message;
-create table message (
+drop table if exists messages cascade;
+create table messages (
   message_id serial primary key,
-  author_id integer not null,
+  user_id integer not null,
   text varchar(5000) not null,
   pub_date integer,
   flagged integer
 );
 
-COPY follower
-FROM '/restore/dumps/follower.csv'
+COPY followers
+FROM '/restore/dumps/followers.csv'
 DELIMITER ','
 CSV HEADER;
 
-COPY "message"
-FROM '/restore/dumps/message.csv'
+COPY messages 
+FROM '/restore/dumps/messages.csv'
 DELIMITER ','
 CSV HEADER;
 
-COPY "user"
-FROM '/restore/dumps/user.csv'
+COPY users
+FROM '/restore/dumps/users.csv'
 DELIMITER ','
 CSV HEADER;
