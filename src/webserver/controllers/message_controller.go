57ec49b179558eeabb2ab3e21eb79554c09774ce
Christoffer Gram <chkg@itu.dk>
2022-03-13 22:10:20 +0100

    Correct UserMessages Impl
    Mixed up the role of user_controller and message_controller

--- src/webserver/controllers/message_controller.go
+++ src/webserver/controllers/message_controller.go
@@ -2,11 +2,14 @@ package controllers
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 
 	"github.com/antonPalmFolkmann/DevOps2022/services"
+	"github.com/antonPalmFolkmann/DevOps2022/utils"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
+	"github.com/jinzhu/gorm"
 )
 
 type IMessage interface {
@@ -38,26 +41,57 @@ func (m *Message) AllMessages(w http.ResponseWriter, r *http.Request) {
 }
 
 func (m *Message) UserMessages(w http.ResponseWriter, r *http.Request) {
-	session, _ := m.store.Get(r, "session-name")
-	if isAuthenticated, found := session.Values["isAuthenticated"].(bool); !isAuthenticated || !found {
-		w.WriteHeader(http.StatusForbidden)
+	vars := mux.Vars(r)
+	if _, found := vars["username"]; !found {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	username := vars["username"]
+
+	id, err := m.users.ReadUserIdByUsername(username)
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		http.Error(w, "There are no messages for that user because they don't exist", http.StatusNotFound)
+		return
+	} else if err != nil {
+		http.Error(w, "There was an error while trying to read the user id", http.StatusInternalServerError)
 		return
 	}
 
-	username := session.Values["username"].(string)
-
-	msgs, err := m.messages.ReadAllMessagesForUsername(username)
+	msgs, err := m.messages.ReadAllMessagesByAuthorId(id)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		http.Error(w, "There was an error while trying to read the messages", http.StatusInternalServerError)
 		return
 	}
 
-	jsonify, _ := json.Marshal(msgs)
+	filteredMsgs := make([]MsgResp, 0)
+	for _, msg := range msgs {
+		author, _ := m.users.ReadUserById(msg.UserID)
+		filteredM := MsgResp{
+			AuthorName: author.Username,
+			Text:       msg.Text,
+			PubDate:    utils.FormatDatetime(int64(msg.PubDate)),
+			Flagged:    msg.Flagged,
+		}
+		filteredMsgs = append(filteredMsgs, filteredM)
+	}
+
+	user, err := m.users.ReadUserByUsername(username)
+	if err != nil {
+		http.Error(w, "There was an error while trying to read the user information", http.StatusInternalServerError)
+	}
+
 	w.WriteHeader(http.StatusOK)
+	resp := MsgsPerUsernameResp{
+		Username: user.Username,
+		Email:    user.Email,
+		Avatar:   "not yet implemented...",
+		Msgs:     filteredMsgs,
+	}
+	jsonify, _ := json.Marshal(resp)
 	w.Write(jsonify)
 }
 
 func (m *Message) SetupRoutes(r *mux.Router) {
-	r.HandleFunc("/", m.UserMessages)
+	r.HandleFunc("/msgs/{username}", m.UserMessages)
 	r.HandleFunc("/public", m.AllMessages)
 }
