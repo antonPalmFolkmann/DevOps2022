7f7b6960179c2916f787bff7c96dbbe9f3335624
Christoffer Gram <chkg@itu.dk>
2022-03-18 20:24:51 +0100

    Add MessageService Tests (Except for FlagMessage)

--- src/webserver/services/message_service.go
+++ src/webserver/services/message_service.go
@@ -11,7 +11,7 @@ type IMessage interface {
 	CreateMessage(username string, text string) error
 	ReadAllMessages(limit int, offset int) ([]storage.MessageDTO, error)
 	ReadAllMessagesByUsername(username string) ([]storage.MessageDTO, error)
-	ReadAllMessagesOfFollowedUsers(username string) ([]storage.MessageDTO ,error)
+	ReadAllMessagesOfFollowedUsers(username string) ([]storage.MessageDTO, error)
 	FlagMessage(ID uint) error
 }
 
@@ -26,7 +26,7 @@ func NewMessageService(db *gorm.DB) *Message {
 func (m *Message) CreateMessage(username string, text string) error {
 	userID := m.getUserIDFromUsername(username)
 	message := storage.Message{UserID: userID, Text: text, PubDate: time.Now().Unix(), Flagged: false}
-	err := m.db.Create(message).Error
+	err := m.db.Create(&message).Error
 	return err
 }
 
@@ -34,9 +34,9 @@ func (m *Message) ReadAllMessages(limit int, offset int) ([]storage.MessageDTO, 
 	var messages = make([]storage.Message, 0)
 	var messageDTOs = make([]storage.MessageDTO, 0)
 	err := m.db.Select("user_id", "text", "pub_date", "flagged").
-				Where("flagged = 0").
-				Find(&messages).Error
-				
+		Where("flagged = 0").
+		Find(&messages).Error
+
 	for _, v := range messages {
 		username := m.getUsernameFromUserID(v.UserID)
 		messageDTO := storage.MessageDTO{UserID: v.UserID, Username: username, Text: v.Text, PubDate: time.Unix(v.PubDate, 0), Flagged: v.Flagged}
@@ -47,12 +47,10 @@ func (m *Message) ReadAllMessages(limit int, offset int) ([]storage.MessageDTO, 
 
 func (m *Message) ReadAllMessagesByUsername(username string) ([]storage.MessageDTO, error) {
 	var messages = make([]storage.Message, 0)
-	var messageDTOs = make([]storage.MessageDTO, 0)
 	ID := m.getUserIDFromUsername(username)
-	err := m.db.Select("user_id", "text", "pub_date", "flagged").
-				Where("user_id = ?", ID).
-				Find(&messages).Error
-				
+	err := m.db.Where("user_id = ?", ID).Find(&messages).Error
+
+	var messageDTOs = make([]storage.MessageDTO, 0)
 	for _, v := range messages {
 		messageDTO := storage.MessageDTO{UserID: v.UserID, Username: username, Text: v.Text, PubDate: time.Unix(v.PubDate, 0), Flagged: v.Flagged}
 		messageDTOs = append(messageDTOs, messageDTO)
@@ -60,34 +58,47 @@ func (m *Message) ReadAllMessagesByUsername(username string) ([]storage.MessageD
 	return messageDTOs, err
 }
 
-func (m *Message) ReadAllMessagesOfFollowedUsers(username string) ([]storage.MessageDTO ,error) {
+func (m *Message) ReadAllMessagesOfFollowedUsers(username string) ([]storage.MessageDTO, error) {
+	var user storage.User
+	err := m.db.Preload("Follows").Where("username = ?", username).Find(&user).Error
+	if err != nil {
+		return nil, err
+	}
+
 	var messages = make([]storage.Message, 0)
+	err = m.db.Where("user_id IN (?)", userToIDs(user.Follows)).Find(&messages).Error
+	if err != nil {
+		return nil, err
+	}
+
 	var messageDTOs = make([]storage.MessageDTO, 0)
-	ID := m.getUserIDFromUsername(username)
-	err := m.db.Select("user_id", "text", "pub_date", "flagged").
-				Where("message_id = ?", ID).
-				Find(&messages).Error
-	
 	for _, v := range messages {
-		messageDTO := storage.MessageDTO{UserID: v.UserID, Username: username, Text: v.Text, PubDate: time.Unix(v.PubDate, 0), Flagged: v.Flagged}
+		author := m.getUsernameFromUserID(v.UserID)
+		messageDTO := storage.MessageDTO{UserID: v.UserID, Username: author, Text: v.Text, PubDate: time.Unix(v.PubDate, 0), Flagged: v.Flagged}
 		messageDTOs = append(messageDTOs, messageDTO)
 	}
 	return messageDTOs, err
+}
+
+func userToIDs(usrs []*storage.User) []uint {
+	IDs := make([]uint, 0)
+	for _, usr := range usrs {
+		IDs = append(IDs, usr.ID)
+	}
+	return IDs
 }
 
 func (m *Message) FlagMessage(ID uint) error {
-	var message storage.Message
-	return m.db.Model(&message).
-				Where("message_id = ?", ID).
-				Update("flagged", 1).Error
+	err := m.db.Raw("UPDATE messages SET flagged=true WHERE messages.id = ?", ID).Error
+	return err
 }
 
 func (m *Message) getUserIDFromUsername(username string) uint {
 	var user storage.User
 	m.db.Unscoped().
-			Where("username = ?", username).
-			Select("id").
-			Find(&user)
+		Where("username = ?", username).
+		Select("id").
+		Find(&user)
 	return user.ID
 }
 
--- src/webserver/services/message_service_test.go
+++ src/webserver/services/message_service_test.go
@@ -1,1 +1,63 @@
 package services_test
+
+import (
+	"testing"
+
+	"github.com/antonPalmFolkmann/DevOps2022/services"
+	"github.com/antonPalmFolkmann/DevOps2022/storage"
+	"github.com/jinzhu/gorm"
+	"github.com/stretchr/testify/assert"
+)
+
+func setUpMessageTestDB() (*gorm.DB, services.IMessage) {
+	db, _ := gorm.Open("sqlite3", ":memory:")
+	storage.Migrate(db)
+
+	userService := services.NewUserService(db)
+	userService.CreateUser("jalle", "jalle@jalle.jalle", "allej")
+	userService.CreateUser("yolo", "yolo@yolo.yolo", "oloy")
+	userService.CreateUser("chrisser", "chrisser@chrisser.chrisser", "swak420")
+
+	userService.Follow("chrisser", "jalle")
+
+	messageService := services.NewMessageService(db)
+	messageService.CreateMessage("jalle", "en hel masse ting")
+	messageService.CreateMessage("yolo", "skriver også en hel masse ting")
+	messageService.CreateMessage("chrisser", "niet")
+
+	return db, messageService
+}
+
+func TestCreateMessage(t *testing.T) {
+	_, service := setUpMessageTestDB()
+
+	msgs, _ := service.ReadAllMessages(10, 10)
+	assert.Len(t, msgs, 3)
+
+	service.CreateMessage("jalle", "new message")
+	actual, _ := service.ReadAllMessages(10, 10)
+	assert.Len(t, actual, 4)
+}
+
+func TestReadAllMessagesReturns3(t *testing.T) {
+	_, service := setUpMessageTestDB()
+
+	actual, _ := service.ReadAllMessages(10, 10)
+	assert.Len(t, actual, 3)
+}
+
+func TestReadAllMessagesByUsername(t *testing.T) {
+	_, service := setUpMessageTestDB()
+
+	actual, _ := service.ReadAllMessagesByUsername("chrisser")
+	assert.Len(t, actual, 1)
+	assert.Equal(t, actual[0].Username, "chrisser")
+}
+
+func TestReadAllMessagesOfFollowedUsers(t *testing.T) {
+	_, service := setUpMessageTestDB()
+
+	actual, _ := service.ReadAllMessagesOfFollowedUsers("chrisser")
+	assert.Len(t, actual, 1)
+	assert.Equal(t, "jalle", actual[0].Username)
+}
