e845f7600bc3e77021236094a0d0a69d8876f501
Christoffer Gram <chkg@itu.dk>
2022-03-20 14:36:48 +0100

    Feature/monitoring (#175)
    * Response Counter on Grafana!
    
    * Add Monitoring!
    
    Co-authored-by: aHjelmgaard <aHjelmgaard@users.noreply.github.com>
    Co-authored-by: Anton Palm Folkmann <antonPalmFolkmann@users.noreply.github.com>
    
    Co-authored-by: aHjelmgaard <aHjelmgaard@users.noreply.github.com>
    Co-authored-by: Anton Palm Folkmann <antonPalmFolkmann@users.noreply.github.com>

--- src/webserver/server.go
+++ src/webserver/server.go
@@ -3,8 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/antonPalmFolkmann/DevOps2022/controllers"
+	"github.com/antonPalmFolkmann/DevOps2022/monitoring"
+	"github.com/antonPalmFolkmann/DevOps2022/services"
+	"github.com/antonPalmFolkmann/DevOps2022/storage"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
 )
 
 func init() {
@@ -12,6 +18,20 @@ func init() {
 }
 
 func main() {
+	time.Sleep(2)
+
+	db := storage.ConnectPsql()
+	storage.Migrate(db)
+
+	userService := services.NewUserService(db)
+	messageService := services.NewMessageService(db)
+
+	store := sessions.NewCookieStore([]byte("supersecret1234"))
+	userController := controllers.NewUserController(userService, messageService, store)
+
 	r := mux.NewRouter()
+	userController.SetupRoutes(r)
+	monitoring.SetupRoutes(r)
+
 	log.Fatalln(http.ListenAndServe(":8080", r))
 }
