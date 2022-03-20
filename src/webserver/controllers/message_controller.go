be975707af12e7fe1830bc60c7ef7fcd43302411
Christoffer Gram <chkg@itu.dk>
2022-03-20 10:56:38 +0100

    Feature/message controller (#157)
    * Beginnings of introducing ORM to database.
    
    Co-authored-by: aHjelmgaard <aHjelmgaard@users.noreply.github.com>
    
    * Finished CRUD for User class.
    
    Co-authored-by: aHjelmgaard <aHjelmgaard@users.noreply.github.com>
    
    * Extracting to Controllers
    Co-authored-by: aHjelmgaard <aHjelmgaard@users.noreply.github.com>
    Co-authored-by: Jakob <Kobo-coder@users.noreply.github.com>
    
    * Changing to Controller format.
    Co-authored-by: aHjelmgaard <aHjelmgaard@users.noreply.github.com>
    Co-authored-by: Jakob <Kobo-coder@users.noreply.github.com>
    
    * Completed UserController and UserService.
    
    Co-authored-by: aHjelmgaard <aHjelmgaard@users.noreply.github.com>
    
    * Finished migrating Message to database.
    
    * Minor edits in startup
    
    * Remove minitwitcopy.db
    
    * Refactor all Get<Model> to Use Struct Types
    
    * Make All Functions in Services Methods
    
    * Make All Files Conform to Naming Style
    
    * Make Folders Follow Naming Convention
    
    * Clean-up storage Package
    
    * Remove Models (Was Moved to schema.go)
    
    * Clean-up services Package
    
    * Format server.go
    
    * Auto-migration worked!
    
    * Make Dumps Fit New Schema
    
    * Make Restore Fit New Schema
    
    * Add Auto-migrating with Restore
    
    * Fix Types in Services based on Models
    
    * WIP: Add MessagesPerUserHandler
    
    Add GET MessagesPerUserHandler.
    
    Reached a roadblock when adding corresponding POST:
    
    Need to create abstraction to guard Controllers against knowing about
    IDs. (DTOs?)
    
    * User Contoller Interface + Remove contollers Folder
    
    * First Attempt at Implementing controllers.IUser
    
    * Set-up Routes in server.go
    
    * Add Models for Request and Response
    
    * Add Models to Ensure Expected JSON Response
    
    * Switch to http.Error
    
    * Correct Timeline Implementation
    
    Mixed up the roles of user_controller and message_controller
    
    * Move ParseUsername to utils
    
    * Add First Draft for message_controller
    
    * Correct UserMessages Impl
    
    Mixed up the role of user_controller and message_controller
    
    * Switch to http.Error
    
    * Quick fix: Remove Duplicate Function in services.IMessage
    
    * Fix Routes
    
    * Go Mod Tidy
    
    * Update UserController to new Service Interfaces
    
    * Add MessageService Tests (Except for FlagMessage)
    
    * All The UserController Tests
    
    * Add Message Controller Tests
    
    Co-authored-by: Anton Palm Folkmann <69316986+antonPalmFolkmann@users.noreply.github.com>
    Co-authored-by: aHjelmgaard <aHjelmgaard@users.noreply.github.com>

---
+++ src/webserver/controllers/message_controller.go
@@ -0,0 +1,107 @@
+package controllers
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/antonPalmFolkmann/DevOps2022/services"
+	"github.com/antonPalmFolkmann/DevOps2022/utils"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
+	"github.com/jinzhu/gorm"
+)
+
+type IMessage interface {
+	AllMessages(w http.ResponseWriter, r *http.Request)
+	UserMessages(w http.ResponseWriter, r *http.Request)
+	SetupRoutes(r *mux.Router)
+}
+
+type Message struct {
+	store    sessions.Store
+	messages services.IMessage
+	users    services.IUser
+}
+
+func NewMessage(store sessions.Store, messages services.IMessage, users services.IUser) *Message {
+	return &Message{store: store, messages: messages, users: users}
+}
+
+func (m *Message) AllMessages(w http.ResponseWriter, r *http.Request) {
+	msgs, err := m.messages.ReadAllMessages(0, 100)
+	if err != nil {
+		http.Error(w, "There was an error while reading messages", http.StatusInternalServerError)
+		return
+	}
+
+	jsonify, _ := json.Marshal(&msgs)
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonify)
+}
+
+func (m *Message) UserMessages(w http.ResponseWriter, r *http.Request) {
+	username, err := utils.ParseUsername(r)
+	if err != nil {
+		http.Error(w, "There is no username", http.StatusNotFound)
+		return
+	}
+
+	user, err := m.users.ReadUserByUsername(username)
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		http.Error(w, "User not found", http.StatusNotFound)
+		return
+	} else if err != nil {
+		http.Error(w, "There was an error while reading user information", http.StatusInternalServerError)
+		return
+	}
+
+	msgs, err := m.messages.ReadAllMessagesByUsername(username)
+	if err != nil {
+		http.Error(w, "There was an error while trying to read the messages", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	resp := MsgsPerUsernameResp{
+		Username: user.Username,
+		Email:    user.Email,
+		Avatar:   "not yet implemented...",
+		Msgs:     msgs,
+	}
+	jsonify, _ := json.Marshal(resp)
+	w.Write(jsonify)
+}
+
+func (m *Message) AddMessage(w http.ResponseWriter, r *http.Request) {
+	session, _ := m.store.Get(r, "session-name")
+
+	if isAuthenticated, found := session.Values["isAuthenticated"].(bool); !isAuthenticated || !found {
+		http.Error(w, "You must be logged in to add a message", http.StatusForbidden)
+		return
+	}
+
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Could not parse the JSON body", http.StatusInternalServerError)
+		return
+	}
+
+	var data AddMsgsReq
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		http.Error(w, "The JSON body is malformed", http.StatusBadRequest)
+		return
+	}
+
+	m.messages.CreateMessage(data.AuthorName, data.Text)
+	w.WriteHeader(http.StatusCreated)
+}
+
+func (m *Message) SetupRoutes(r *mux.Router) {
+	r.HandleFunc("/msgs/{username}", m.UserMessages)
+	r.HandleFunc("/public", m.AllMessages)
+	r.HandleFunc("/add_message", m.AddMessage)
+}
