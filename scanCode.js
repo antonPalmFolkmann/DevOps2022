841a82907dbe061927bfdcc18efb280a60c8c597
Oskar Breindahl <osbr@itu.dk>
2022-03-29 09:12:14 +0200

    Final commit for now. yml added. Will attempt main merge

--- scanCode.js
+++ scanCode.js
@@ -1,20 +1,31 @@
-const scanResults = require('./sample.json')
+const scanLicenseResults = require('./license.json')
+const scanSourceResults  = require('./src.json')
 let nrOfLicensesFound = 0
 
 console.log('Checking ScanCode result for licenses')
 
-for (var i = 0; i < scanResults.files.length; i++) {
-    if (scanResults.files[i].licenses.length > 0) {
-        console.log('License(s) found in file: ' + scanResults.files[i].base_name)
-        for (var j = 0; j < scanResults.files[i].licenses.length; i++) {
+for (var i = 0; i < scanLicenseResults.files.length; i++) {
+    if (scanLicenseResults.files[i].licenses.length > 0) {
+        console.log('License(s) found in file: ' + JSON.stringify(scanLicenseResults.files[i].path))
+        for (var j = 0; j < scanLicenseResults.files[i].licenses.length; j++) {
             nrOfLicensesFound++
-            console.log(scanResults.files[i].licenses[j])
+            console.log(scanLicenseResults.files[i].licenses[j].name)
+        }
+    }
+}
+
+for (var i = 0; i < scanSourceResults.files.length; i++) {
+    if (scanSourceResults.files[i].licenses.length > 0) {
+        console.log('License(s) found in file: ' + JSON.stringify(scanSourceResults.files[i].path))
+        for (var j = 0; j < scanSourceResults.files[i].licenses.length; j++) {
+            nrOfLicensesFound++
+            console.log(scanSourceResults.files[i].licenses[j].name)
         }
     }
 }
 
 if (nrOfLicensesFound > 0) {
-    console.log('Check complete. ' + nrOfLicensesFound + 'licenses found.')
+    console.log('Check complete. ' + nrOfLicensesFound + ' licenses found.')
 } else {
     console.log('Check complete. No licenses found.')
 }
