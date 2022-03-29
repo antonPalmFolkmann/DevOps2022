40150d7de959f1cc01410fd4ad0986854e242e35
Anton Palm Folkmann <69316986+antonPalmFolkmann@users.noreply.github.com>
2022-03-29 11:10:47 +0200

    Fixed for loop to for of loop.

--- scanCode.js
+++ scanCode.js
@@ -4,22 +4,22 @@ let nrOfLicensesFound = 0
 
 console.log('Checking ScanCode result for licenses')
 
-for (let i = 0; i < scanLicenseResults.files.length; i++) {
-    if (scanLicenseResults.files[i].licenses.length > 0) {
-        console.log('License(s) found in file: ' + JSON.stringify(scanLicenseResults.files[i].path))
-        for (let j = 0; j < scanLicenseResults.files[i].licenses.length; j++) {
+for (let index of scanLicenseResults.files.keys()) {
+    if (scanLicenseResults.files[index].licenses.length > 0) {
+        console.log('License(s) found in file: ' + JSON.stringify(scanLicenseResults.files[index].path))
+        for (let jndex of scanLicenseResults.files[index].licenses.keys()) {
             nrOfLicensesFound++
-            console.log(scanLicenseResults.files[i].licenses[j].name)
+            console.log(scanLicenseResults.files[index].licenses[jndex].name)
         }
     }
 }
 
-for (let i = 0; i < scanSourceResults.files.length; i++) {
-    if (scanSourceResults.files[i].licenses.length > 0) {
-        console.log('License(s) found in file: ' + JSON.stringify(scanSourceResults.files[i].path))
-        for (let j = 0; j < scanSourceResults.files[i].licenses.length; j++) {
+for (let index of scanSourceResults.files.keys()) {
+    if (scanSourceResults.files[index].licenses.length > 0) {
+        console.log('License(s) found in file: ' + JSON.stringify(scanSourceResults.files[index].path))
+        for (let jndex of scanSourceResults.files[index].licenses.keys()) {
             nrOfLicensesFound++
-            console.log(scanSourceResults.files[i].licenses[j].name)
+            console.log(scanSourceResults.files[index].licenses[jndex].name)
         }
     }
 }
