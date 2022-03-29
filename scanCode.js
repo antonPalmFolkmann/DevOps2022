5c292738a7749c5b2e3a91f6e6f27363d3e89ac4
Anton Palm Folkmann <69316986+antonPalmFolkmann@users.noreply.github.com>
2022-03-29 10:59:29 +0200

    Fixed minor codesmell "var" to "let".
    Co-authored-by: aHjelmgaard <aHjelmgaard@users.noreply.github.com>

--- scanCode.js
+++ scanCode.js
@@ -4,20 +4,20 @@ let nrOfLicensesFound = 0
 
 console.log('Checking ScanCode result for licenses')
 
-for (var i = 0; i < scanLicenseResults.files.length; i++) {
+for (let i = 0; i < scanLicenseResults.files.length; i++) {
     if (scanLicenseResults.files[i].licenses.length > 0) {
         console.log('License(s) found in file: ' + JSON.stringify(scanLicenseResults.files[i].path))
-        for (var j = 0; j < scanLicenseResults.files[i].licenses.length; j++) {
+        for (let j = 0; j < scanLicenseResults.files[i].licenses.length; j++) {
             nrOfLicensesFound++
             console.log(scanLicenseResults.files[i].licenses[j].name)
         }
     }
 }
 
-for (var i = 0; i < scanSourceResults.files.length; i++) {
+for (let i = 0; i < scanSourceResults.files.length; i++) {
     if (scanSourceResults.files[i].licenses.length > 0) {
         console.log('License(s) found in file: ' + JSON.stringify(scanSourceResults.files[i].path))
-        for (var j = 0; j < scanSourceResults.files[i].licenses.length; j++) {
+        for (let j = 0; j < scanSourceResults.files[i].licenses.length; j++) {
             nrOfLicensesFound++
             console.log(scanSourceResults.files[i].licenses[j].name)
         }
