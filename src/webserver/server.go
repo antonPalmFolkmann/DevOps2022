fe9d0503b66c3142bf262e745d7986ea18234922
Chris <chkg@itu.dk>
2022-04-05 09:25:20 +0200

    Add Logging Stmts to Services

--- src/webserver/server.go
+++ src/webserver/server.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"log"
 	"net/http"
-	"time"
+	"os"
 
 	"github.com/antonPalmFolkmann/DevOps2022/controllers"
 	"github.com/antonPalmFolkmann/DevOps2022/monitoring"
@@ -11,20 +10,16 @@ import (
 	"github.com/antonPalmFolkmann/DevOps2022/storage"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
+	"github.com/sirupsen/logrus"
 )
 
-func init() {
-
-}
-
 func main() {
-	time.Sleep(2)
-
+	log := logrus.New()
 	db := storage.ConnectPsql()
 	storage.Migrate(db)
 
-	userService := services.NewUserService(db)
-	messageService := services.NewMessageService(db)
+	userService := services.NewUserService(db, log)
+	messageService := services.NewMessageService(db, log)
 	simulatorService := services.NewSimulatorService()
 
 	store := sessions.NewCookieStore([]byte("supersecret1234"))
@@ -33,22 +28,17 @@ func main() {
 	serviceController := controllers.NewSimulator(messageService, userService, simulatorService)
 
 	go func() {
-		log.Println("Starting the simulator router...")
+		log.Trace("Starting the simulator router")
 		r := mux.NewRouter()
 		monitoring.SetupRoutes(r)
 		serviceController.SetupRoutes(r)
 		http.ListenAndServe(":8081", r)
 	}()
 
-	log.Println("Starting the minitwit router...")
+	log.Trace("Starting the minitwit router")
 	r := mux.NewRouter()
 	userController.SetupRoutes(r)
 	monitoring.SetupRoutes(r)
 	messageController.SetupRoutes(r)
 	http.ListenAndServe(":8080", r)
 }
-
-// func main() {
-// 	r := mux.NewRouter()
-// 	log.Fatalln(http.ListenAndServe(":8080", r))
-// }
--- src/webserver/services/user_service.go
+++ src/webserver/services/user_service.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/antonPalmFolkmann/DevOps2022/storage"
 	"github.com/jinzhu/gorm"
+	"github.com/sirupsen/logrus"
 )
 
 type IUser interface {
@@ -21,55 +22,74 @@ type IUser interface {
 }
 
 type User struct {
-	db *gorm.DB
+	db  *gorm.DB
+	log *logrus.Logger
 }
 
-func NewUserService(db *gorm.DB) *User {
-	return &User{db: db}
+func NewUserService(db *gorm.DB, log *logrus.Logger) *User {
+	return &User{db: db, log: log}
 }
 
 func (u *User) CreateUser(username string, email string, password string) error {
+	u.log.Trace("Creating a user")
+
 	pwHash := u.hash(password)
 	user := storage.User{Username: username, Email: email, PwHash: pwHash}
 	err := u.db.Create(&user).Error
+	u.log.Debug("Created a user on the database")
 	return err
 }
 
 func (u *User) ReadAllUsers() ([]storage.User, error) {
+	u.log.Trace("Reading all users")
+
 	var users []storage.User
 	err := u.db.Select([]string{"id", "username", "email", "pw_hash"}).
 		Find(&users).Error
+
 	return users, err
 }
 
 func (u *User) ReadUserByUsername(username string) (storage.User, error) {
+	u.log.Trace("Reading user by username")
+
 	var user storage.User
 	err := u.db.Where("username = ?", username).
-				Select([]string{"id", "username", "email", "pw_hash"}).
-				Find(&user).Error
+		Select([]string{"id", "username", "email", "pw_hash"}).
+		Find(&user).Error
+	u.log.Debug("Read user database")
 	return user, err
 }
 
 func (u *User) ReadUserIdByUsername(username string) (uint, error) {
+	u.log.Trace("Reading user ID by username")
+
 	var user storage.User
 	err := u.db.Where("username = ?", username).
-				Select("id").
-				Find(&user).Error
+		Select("id").
+		Find(&user).Error
+	u.log.Debug("Reader user from database, returning their ID")
 	return user.ID, err
 }
 
 func (u *User) hash(password string) string {
+	u.log.Trace("Hashing a password")
+
 	hash := md5.New()
 	io.WriteString(hash, password)
 	return fmt.Sprintf("%x", hash.Sum(nil))
 }
 
 func (u *User) Follow(username string, whomname string) error {
+	u.log.Trace("Following a user")
+
 	var user storage.User
 	err := u.db.
 		Where("username = ?", username).
 		First(&user).Error
+	u.log.Debug("Read the user wanting to follow from the database")
 	if err != nil {
+		u.log.Warn("Unable to find the user wanting to follow another user with error: ", err.Error())
 		return err
 	}
 
@@ -77,7 +97,9 @@ func (u *User) Follow(username string, whomname string) error {
 	err = u.db.
 		Where("username = ?", whomname).
 		First(&whom).Error
+	u.log.Debug("Read the user to be followed from the database")
 	if err != nil {
+		u.log.Warn("Unable to find the user that should be followed with error: ", err.Error())
 		return err
 	}
 
@@ -87,13 +109,17 @@ func (u *User) Follow(username string, whomname string) error {
 }
 
 func (u *User) Unfollow(username string, whomname string) error {
+	u.log.Trace("A user is unfollowing another user")
+
 	user, err := u.ReadUserByUsername(username)
 	if err != nil {
+		u.log.Warn("Unable to find the user wanting to unfollow another user with error: ", err.Error())
 		return err
 	}
 
 	whom, err := u.ReadUserByUsername(whomname)
 	if err != nil {
+		u.log.Warn("Unable to find the user that should be unfollowed with error: ", err.Error())
 		return err
 	}
 
@@ -102,16 +128,20 @@ func (u *User) Unfollow(username string, whomname string) error {
 }
 
 func (u *User) IsPasswordCorrect(username string, password string) bool {
+	u.log.Trace("Checking if a password is correct")
+
 	passwordHashed := u.hash(password)
 	usr, err := u.ReadUserByUsername(username)
 	if err != nil {
 		return false
 	}
-	
+
 	return (usr.PwHash == passwordHashed)
 }
 
 func (u *User) IsUsernameTaken(username string) bool {
+	u.log.Trace("Checking if a username is taken")
+
 	_, err := u.ReadUserByUsername(username)
-	return err == nil 
+	return err == nil
 }
