dbd972a0196e1de514bb920c417fd5738a2c5167
Chris <chkg@itu.dk>
2022-05-10 10:43:56 +0200

    Add Some Todos

--- src/scripts/docker/build.sh
+++ src/scripts/docker/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# TODO:
+# npm run build /path/to/package.json/ --output=./client/build
+
 docker build -f docker/webserver.Dockerfile -t antonfolkmann/minitwit .
 docker build -f docker/db.Dockerfile -t antonfolkmann/minitwitdb .
